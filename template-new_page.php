9a9c44d29155161ac91034165d61181307694e2b
simass <simasjarukaitis@gmail.com>
2022-02-21 19:52:45 +0200

    kazkas kazkur kazkaip kazka

---
+++ template-new_page.php
@@ -0,0 +1,338 @@
+<?php /* Template Name: template-new_page */ ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Individualių namų konstrukcijų projektavimo darbai</title>
+    <link rel="stylesheet" href="/wp-content/themes/llorix-one-lite/new-theme-content/psl1.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div class="navbar">
+        <a href="#pradzia"><img src="/wp-content/themes/llorix-one-lite/new-theme-content/images/Lengva-sija-Baltas.png"></a>
+        <ul>
+            <li><a href="#kontaktai">KONTAKTAI</a></li>
+            <li><a href="#karjera">KARJERA</a></li>
+            <li><a href="#parengti-darbai">PARENGTI DARBAI</a></li>
+            <li><a href="#paslaugos">PASLAUGOS</a></li>
+            <li><a href="#konsultavimas">KONSULTAVIMAS</a></li>
+            <li><a href="#planavimas">PLANAVIMAS</a></li>
+            <li><a href="#apie_imone">APIE</a></li>
+        </ul>
+    </div>
+    <section class="header" id="pradzia">
+        <div class="text-box">
+            <h1>Individualių Namų Konstrukcijų Projektavimo Darbai</h1>
+            <p>Mes sėkmingai vykdome veiklą jau: <br> </p>
+            <div class="timer">
+                <p id="timer"></p>
+            </div>
+            <a href="#apie_imone" class="herro-btn">Apie Įmonę</a>
+        </div>
+    </section>
+
+
+
+    <!--Pagrindinis-->
+
+
+    <section class="apie-imone" id="apie_imone">
+        <div class="eilute">
+            <div class="ivadas-1">
+                <h1 id="apie_imone">Apie Įmonę</h1>
+                <p style="text-align: justify">
+                    Savo veiklą jau kelerius metus orientuojame tik į individualių namų projektų rengimą.
+                    Būtent individualių namų konstrukcijų ir energinio vertinimo projektavimo srityse esame sukaupę
+                    nemažą patirtį, kuria maloniai galime pasidalinti su Jumis.<br /><br />
+                    Įmonės vadovas – inžinierius Anton Budko, dirbantis konstruktoriumi daugiau nei 10 metų.
+                </p>
+            </div>
+            <div class="ivadas-2">
+                <img src="/wp-content/themes/llorix-one-lite/new-theme-content/images/civil-engineering-company.jpg" height="500" width="900" />
+            </div>
+        </div>
+    </section>
+
+    <p style="page-break-after: always;">&nbsp; </p>
+    <p style="page-break-before: always;">&nbsp;</p>
+
+    <!--tekstas-->
+    <section class="Planuokis" id="planavimas">
+        <div class="tekstas-1">
+            <h1> Planuokite Statybas Iš Anksto! </h1>
+            <p>
+                Žinoma, svarbu iš anksto įsivaizduoti planuojamos statybos biudžetą.
+                Patyrę statybininkai skaičiuoja sąmatas ir pateikia savo pasiūlymus.
+                Turint po ranka konstrukcinės dalies projektą, tiek rangovams, tiek ir Užsakovui paprasčiau ne tik
+                įvertinti statybos sąmatą,
+                bet ir apibrėžti technologinio proceso sudėtingumą, jo kiekį ir kainą. Be to, priduodant pastatytą
+                statinį,
+                yra privaloma pateikti darbo projektą, todėl konstrukcinė dalis yra neatsiejama projekto dalis, kuria
+                būtina pasirūpinti iš anksto.
+            </p>
+        </div>
+    </section>
+
+    <p style="page-break-after: always;">&nbsp;</p>
+
+    <section class="Konsultuokis" id="konsultavimas">
+        <div class="eilute-1">
+            <div class="tekstas-2">
+                <h1> Konsultuokites Su Konstruktoriumi </h1>
+                <p style="text-align: justify">
+                    Svarbu iš anksto apibrėžti pagrindinius projekto statybinių konstrukcijų sudedamuosius elementus su
+                    Užsakovu.
+                    Taip konstruktorius suformuoja sau užduotį projektavimui. Svarbu aptarti esminius aspektus: pamatų
+                    tipo parinkimą
+                    (ar tai juostinis pamatas ar poliniai gręžiamieji pamatai), blokelio pasirinkimą (akytojo betono,
+                    keramikiniai, silikatiniai),
+                    fasado įrengimo sistemos panaudojimą projekte (gali būti tinkuojamas/vėdinamas), stogo dangos tipą
+                    (skarda/čerpės).<br />
+                </p>
+            </div>
+            <div class="photo-1">
+                <img src="/wp-content/themes/llorix-one-lite/new-theme-content/images/shutterstock_1662218761.jpg" height="500" width="100%" />
+            </div>
+        </div>
+    </section>
+    <p style="page-break-after: always;">&nbsp;</p>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <section class="slide-text" id="paslaugos">
+        <div class="img-slider">
+            <div class="tekstas-3">
+                <h1> Konstrukcinės Dalies Projekto Sudėtis Ir Kaina </h1>
+                <p>
+                    Konstrukcinės dalies projektas susideda iš dviejų pagrindinių dalių – grafinės ir skaitinės.
+                    Skaitinėje dalyje pateikiamos techninės specifikacijos, aiškinamieji raštai.
+                    Grafinėje dalyje atvaizduojami visi pagrindiniai technologiniai statybos etapai
+                    (pamatų, sienų, stogo planai), pateikiami kiekių žiniaraščiai, nurodomi gelžbetoninių elementų
+                    armavimo pjūviai,
+                    detalizuojamos metalo konstrukcijos, pateikiamos sienų išklotinės, išsprendžiami
+                    pagrindiniai atitvarų šiltinimo mazgai,
+                    nurodant šilumos perdavimo koeficientų projektuojamasias reikšmes.
+                    <br /><br />
+                    Projekto architektūrinė dalis padeda konstruktoriui suvokti projekto sudėtingumą, apibrėžti jo
+                    kainą ir terminus.
+                    Laukiame elektroniniu paštu atsiųstus Jūsų projektuojamo namo architektūrinius brėžinius,
+                    pateiksime kainą ir artimiausią atlikimo terminą.
+                </p>
+            </div>
+            <div class="tekstas-3">
+                <h1> Apie Pastatų Energinį Vertinimą </h1>
+                <p>
+                    Kas yra pastato energinis vertinimas? Kodėl jis reikalingas projekto rengimo stadijoje?
+                    Kryptingas energijos vartojimo mažinimas ir atsinaujinančių išteklių energijos naudojimas
+                    pastatų sektoriuje yra svarbios priemonės,
+                    būtinos sumažinti energinę priklausomybę ir šiltnamio efektą, sukeliančių dūjų emisiją.
+                    Dar 2009 metais Europos Parlamento ir Tarybos sprendimu Nr. 406/2009/EB buvo nurodyti valstybėms
+                    įsipareigojimai apie šiltnamio efektą sukeliančių dujų emesijų sumažinimą,
+                    nustatomi nacionaliniai privalomi CO2 kiekio mažinimo tikslai, kurių siekiant itin svarbus
+                    energijos vartojimo efektyvumas pastatų sektoriuje.
+                    Pastato energinis vertinimas parodo, kiek pastatas suvartoja energijos.
+                    Tokio projekto pagrindinis tiklas – parengti ir įvertinti projektuojamo pastato atitiktį
+                    energinio naudingumo klasės (A, A+, A++) reikalavimams ir pateikti skaičiavimo rezultatus.
+                    Energinio vertinimo dalį projekto vadovas pateikia projekto bendroje dalyje.
+                </p>
+            </div>
+            <div class="tekstas-3">
+                <h1> Sužinokite Energinio Naudingumo Projekto Kainą </h1>
+                <p>
+                    (Atsiųskite el. paštu projektuojamo namo aukšto architektūrinius planus, pjūvį ir stogo
+                    architektūrinį planą pdf formatu ir mes įvertinę projekto sudėtingumą ir apimtį kuo greičiau
+                    pateiksime Jums pasiūlymą).
+
+                    Ypač patogu ne tik Užsakovui, bet ir architektui, kai energinio vertinimo dalį ir konstrukcijų
+                    projekto parengimo dalį atlieka konstruktorius. Tada nereikia pateikti užduotį skirtingiems
+                    projekto rengėjams, o viską patikėti vienam specialistui.
+                    Namo energijos išsaugojimui įtakos turi du pagrindiniai aspektai, kuriuos ir apibrėžia energinio
+                    vertinimo dalies rengėjas: namo konstrukcijų atitvarų plotai ir namo inžinerinės sisitemos
+                    projektavimas.
+                    Atitvarų apšiltinimo įrengimo detalės projektuojamos ir atvaizduojamos projekto konstrukcinėje
+                    dalyje pagal energinio vertinimo iš anksto suprojektuotus šiluminius rodiklius. Bet
+                    konstrukciniai sprendimai taip pat gali paveikti fasado apšiltinimo plotų kiekius, atskiras
+                    šiluminių tiltelių konstrukcijas. Todėl, ruošiant namo energinio vertinimo dalį, būtų naudinga
+                    atsižvelgti ir į numatytus atskirus konstrukcinius sprendinius.
+                </p>
+            </div>
+            <div class="navigation">
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            </div>
+        </div>
+        <div class="dot-container">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+        </div>
+    </section>
+    <section class="parengti-darbai" id="parengti-darbai">
+        <div id="myBtnContainer">
+            <button class="button" id="2017" onclick="show_2017()"><span>2017</span></button>
+            <button class="button" id="2018" onclick="show_2018()"><span>2018</span></button>
+            <button class="button" id="2019" onclick="show_2019()"><span>2019</span></button>
+            <button class="button" id="2020" onclick="show_2020()"><span>2020</span></button>
+            <button class="button" id="2021" style="background-color:rgb(226,73,2);" onclick="show_2021()"><span>2021</span></button>
+        </div>
+        <div class="gif-slider">
+            <div class="col-5" >
+                <img id="gif_place" src="/wp-content/themes/llorix-one-lite/new-theme-content/images/2021.gif" />
+            </div>
+        </div>
+    </section>
+    <section class="karjera" id="karjera">
+        <div class="row2">
+            <img src="/wp-content/themes/llorix-one-lite/new-theme-content/images/Talk to us.gif" />
+        </div>
+    </section>
+
+
+    <section class="icons" id="kontaktai">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"
+            integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ"
+            crossorigin="anonymous"></script>
+        <div class="row">
+            <ul>
+                <li>
+                    <i class="far fa-address-card"></i>
+                    <p>Anton Budko</p>
+                </li>
+                <li>
+                    <i class="fas fa-mobile-alt"></i>
+                    <p><a href="tel:+37060455753">+37060455753</a></p>
+                </li>
+                <li>
+                    <i class="far fa-envelope"></i>
+                    <p><a href="mailto:info@lengvasija.lt">info@lengvasija.lt</a></p>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <section class="bottom">
+        <p>© Lengva sija</p>
+    </section>
+
+    <!--JavaScript-->
+    <!--slideshow-->
+    <script>
+        var slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("tekstas-3");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) { slideIndex = 1 }
+            if (n < 1) { slideIndex = slides.length }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
+    </script>
+    <!--gifs-->
+    <script>
+        var elements = document.getElementsByClassName("col");
+        var i;
+        function one() {
+            for (i = 0; i < elements.length; i++) {
+                elements[i].style.flex = "100%";
+            }
+        }
+    </script>
+    <!--meniu-->
+    <script>
+
+        var navLinks = document.getElementById("navLinks");
+        function showMenu() {
+            navLinks.style.right = "0";
+        }
+        function hideMenu() {
+            navLinks.style.right = "-200px";
+        }
+
+    </script>
+    <!--clock-->
+    <script>
+        var countDownDate = new Date("Feb 14, 2017 9:00:00").getTime();
+        var x = setInterval(function () {
+            var now = new Date().getTime();
+            var distance = now - countDownDate;
+            var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365.25));
+            var days = Math.floor(distance % (1000 * 60 * 60 * 24 * 365.25) / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            //if we want to use them
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            document.getElementById("timer").innerHTML = years + " m. " + days + " d. " + hours + " val. ";
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("timer").innerHTML = "Are you doing time travel?";
+            }
+        }, 1000);
+    </script>
+    <script>
+        function show_2017() {
+            document.getElementById("gif_place").src="/wp-content/themes/llorix-one-lite/new-theme-content/images/2017.gif";
+            document.getElementById("2017").style.backgroundColor="#e24902";
+            document.getElementById("2018").style.backgroundColor="#008bff";
+            document.getElementById("2019").style.backgroundColor="#008bff";
+            document.getElementById("2020").style.backgroundColor="#008bff";
+            document.getElementById("2021").style.backgroundColor="#008bff";
+        }
+        function show_2018() {
+            document.getElementById("gif_place").src="/wp-content/themes/llorix-one-lite/new-theme-content/images/2018.gif";
+            document.getElementById("2017").style.backgroundColor="#008bff";
+            document.getElementById("2018").style.backgroundColor="#e24902";
+            document.getElementById("2019").style.backgroundColor="#008bff";
+            document.getElementById("2020").style.backgroundColor="#008bff";
+            document.getElementById("2021").style.backgroundColor="#008bff";
+        }
+        function show_2019() {
+            document.getElementById("gif_place").src="/wp-content/themes/llorix-one-lite/new-theme-content/images/2019.gif";
+            document.getElementById("2017").style.backgroundColor="#008bff";
+            document.getElementById("2018").style.backgroundColor="#008bff";
+            document.getElementById("2019").style.backgroundColor="#e24902";
+            document.getElementById("2020").style.backgroundColor="#008bff";
+            document.getElementById("2021").style.backgroundColor="#008bff";
+        }
+        function show_2020() {
+            document.getElementById("gif_place").src="/wp-content/themes/llorix-one-lite/new-theme-content/images/2020.gif";
+            document.getElementById("2017").style.backgroundColor="#008bff";
+            document.getElementById("2018").style.backgroundColor="#008bff";
+            document.getElementById("2019").style.backgroundColor="#008bff";
+            document.getElementById("2020").style.backgroundColor="#e24902";
+            document.getElementById("2021").style.backgroundColor="#008bff";
+        }
+        function show_2021() {
+            document.getElementById("gif_place").src="/wp-content/themes/llorix-one-lite/new-theme-content/images/2021.gif";
+            document.getElementById("2017").style.backgroundColor="#008bff";
+            document.getElementById("2018").style.backgroundColor="#008bff";
+            document.getElementById("2019").style.backgroundColor="#008bff";
+            document.getElementById("2020").style.backgroundColor="#008bff";
+            document.getElementById("2021").style.backgroundColor="#e24902";
+        }
+    </script>
+</body>
+
+</html>
